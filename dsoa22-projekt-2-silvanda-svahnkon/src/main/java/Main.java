65ac2470822d0399d3ddcab3c840878362477061
konrad <svahnkon@arcada.fi>
2022-03-03 21:45:50 +0200

    INLÄMNING / alting verkar funka

--- dsoa22-projekt-2-silvanda-svahnkon/src/main/java/Main.java
+++ dsoa22-projekt-2-silvanda-svahnkon/src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 	private static ArrayList<CharPrevalance> prev = new ArrayList<CharPrevalance>();
 	private static ArrayList<CharPrevalance> prev2 = new ArrayList<CharPrevalance>();
-	private static ArrayList<CharPrevalance> prev3 = new ArrayList<CharPrevalance>(); // ;)
+	private static ArrayList<CharPrevalance> prev3 = new ArrayList<CharPrevalance>(); //  ;)
 	private static String input;
 	public static void main(String[] args) {
 		try (Scanner scan = new Scanner(System.in)) {
